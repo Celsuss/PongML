5de2ea6349282bfc8618868b1be2e91c83a9d46d
Jens Lordén <jenslorden@gmail.com>
2018-06-30 23:50:10 +0200

    Updated ScoreManager.

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour {
+	[SerializeField] Text m_ScoreText;
+	List<int> m_Score;
 
 	// Use this for initialization
 	void Start () {
-		
+		m_Score.Add(0);
+		m_Score.Add(0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void AddScore(int player, int score){
+
 	}
 }
