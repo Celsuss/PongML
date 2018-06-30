328908d122a300e02c80bdc248bdffc926402806
Jens Lordén <jenslorden@gmail.com>
2018-06-30 17:46:58 +0200

    Updated car agent and added an agent rewards component.

---
+++ Assets/Scripts/AgentRewards.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentRewards : MonoBehaviour {
+	float m_Reward;
+	public float Reward{ get{
+			float reward = m_Reward;
+			m_Reward = 0;
+			return reward;
+		}}
+
+	// Use this for initialization
+	void Start () {
+		m_Reward = 0;
+	}
+
+	public void GiveReward(float reward){
+		m_Reward += reward;
+	}
+
+	public void Reset(){
+		m_Reward = 0;
+	}
+}
--- Assets/Scripts/PadAgent.cs
+++ Assets/Scripts/PadAgent.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 using UnityEngine.Assertions;
 
 public class PadAgent : Agent {
-
 	[SerializeField] Transform m_Target;
+	PadMovement m_Movement;
+	AgentRewards m_Rewards;
 
 	// Use this for initialization
 	void Start () {
 		Assert.IsNotNull(m_Target);
+		m_Movement = GetComponent<PadMovement>();
+		Assert.IsNotNull(m_Movement);
+		m_Rewards = GetComponent<AgentRewards>();
+		Assert.IsNotNull(m_Rewards);
 	}
 	
 	public override void AgentReset(){
-
+		m_Rewards.Reset();
 	}
 
 	public override void CollectObservations(){
@@ -21,29 +26,19 @@ public class PadAgent : Agent {
 		
 		// // Relative position
 		AddVectorObs(transform.position.x/5);
-		AddVectorObs(transform.position.z/5);
+		//AddVectorObs(transform.position.z/5);
 	}
 
 	public override void AgentAction(float[] vectorAction, string textAction){
 		// Time penalty
 		AddReward(-0.05f);
 
-		// Car damage penalty
-		// float damage = m_Damage.GetDamageAndReset();
-		// AddReward(-damage);
+		// Add all other rewards
+		float reward = m_Rewards.Reward;
+		AddReward(reward);
 
-		// // Car rewards
-		// float reward = m_Reward.GetRewardAndReset();
-		// AddReward(reward);
-
-		// // Actions, size = 2
-
-		// float force = Mathf.Clamp(vectorAction[0], -1, 1);
-		// m_Controller.Throttle(force);
-
-		// float steer = 0;
-		// steer = Mathf.Clamp(vectorAction[1], -1, 1);
-		// m_Controller.Steer(steer);
-
+		// Actions, size = 1
+		float direction = Mathf.Clamp(vectorAction[0], -1, 1);
+		m_Movement.Move(direction);
 	}
 }
